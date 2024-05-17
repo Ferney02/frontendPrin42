305986e4d1e6105516a11e633f5d6ed49b2b7ce0
Ferney <jaiverbenavides81@gmail.com>
2024-05-17 16:15:52 -0500

    Actua5

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Menu = () => {
-    return ( 
+    return (
         <nav className="mt-2">
             <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li className="nav-item">
@@ -21,9 +21,17 @@ const Menu = () => {
                         </p>
                     </Link>
                 </li>
+                <li className="nav-item">
+                    <Link to={"/proveedors"} className="nav-link">
+                        <i className="nav-icon fas fa-edit" />
+                        <p>
+                            Proveedores
+                        </p>
+                    </Link>
+                </li>
             </ul>
         </nav>
-     );
+    );
 }
- 
+
 export default Menu;
